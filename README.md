2da585004e58390dd1c0d681e6f0ba2aaa38d632
jcuga <jcuga@users.noreply.github.com>
2022-07-09 17:29:33 -0400

    first pass at args

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # hax
 cmdline hex editor/pocket-knife
+
+## Build
+```
+go fmt ./... && go vet ./... && go build -o hax main.go
+```
+
+## Use
+TODO: different input/output methods and modes.
+TODO: different functions.
+TODO: example of --pretty with less -R
+
---
+++ main.go
@@ -0,0 +1,118 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+)
+
+func main() {
+	// Input is either: file, --str arg, or stdin
+	var inFilename string
+	flag.StringVar(&inFilename, "file", "", "Filename to read from. Parsed as raw by default (change with --input).")
+	flag.StringVar(&inFilename, "f", "", "")
+	var inputStr string
+	flag.StringVar(&inputStr, "str", "", "String input instead of file/stdin. Parsed as hex by default.")
+	flag.StringVar(&inputStr, "s", "", "")
+
+	// Input/output can be raw binary, hex string, base64 string, binary string,
+	// or copy+paste of hax output.
+	var inMode string
+	flag.StringVar(&inMode, "input", "", "Input mode. See I/O Modes section for options.")
+	flag.StringVar(&inMode, "i", "", "")
+	var outMode string
+	flag.StringVar(&outMode, "output", "display", "Output mode. See I/O Modes section for options (default: display).")
+	flag.StringVar(&outMode, "o", "display", "")
+
+	// Optional input limit/offset. This can be in decimal or hex.
+	var offset string
+	flag.StringVar(&offset, "offset", "", "Input offset in bytes (dec or hex).")
+	flag.StringVar(&offset, "n", "", "")
+	var limit string
+	flag.StringVar(&limit, "limit", "", "Input limit in bytes (dec or hex).")
+	flag.StringVar(&limit, "l", "", "")
+
+	// Customize display mode output:
+	var colWidth string
+	flag.StringVar(&colWidth, "width", "16", "Column Width: how many bytes to display per row (default: 16).")
+	flag.StringVar(&colWidth, "w", "16", "")
+	var alwaysPretty bool
+	flag.BoolVar(&alwaysPretty, "pretty", false, "Always pretty-print/style output.")
+	flag.BoolVar(&alwaysPretty, "y", false, "")
+	var pageSie string
+	flag.StringVar(&pageSie, "page", "0", "Page size to break up output (default 0 means no page breaks).")
+	flag.StringVar(&pageSie, "p", "0", "")
+	var quiet bool
+	flag.BoolVar(&quiet, "no-ascii", false, "Skip outputting ascii below each row of bytes.")
+	flag.BoolVar(&quiet, "q", false, "")
+
+	flag.Usage = func() {
+		w := flag.CommandLine.Output() // may be os.Stderr - but not necessarily
+		// NOTE: custom stuff before
+		fmt.Fprintf(w, "HAX - binary pocket knife.\n\n")
+		fmt.Fprintf(w, "Usage:\thax [input options] [output options]\n")
+
+		fmt.Fprintf(w, "\nInput Options:\n")
+		f := flag.Lookup("file")
+		fmt.Fprintf(w, "\t-f, --%s\t%s\n", f.Name, f.Usage)
+		f = flag.Lookup("str")
+		fmt.Fprintf(w, "\t-s, --%s\t%s\n", f.Name, f.Usage)
+		f = flag.Lookup("input")
+		fmt.Fprintf(w, "\t-i, --%s\t%s\n", f.Name, f.Usage)
+		fmt.Fprintf(w, "\t\t\tIf stdin or --file input, defaults to raw, if --str defaults to hex.\n")
+		fmt.Fprintf(w, "\t\t\t--input=display can be used with --str set to copy+paste of prev output\n")
+		fmt.Fprintf(w, "\t\t\tto re-feed formatted output.\n")
+
+		f = flag.Lookup("offset")
+		fmt.Fprintf(w, "\t-n, --%s\t%s\n", f.Name, f.Usage)
+		f = flag.Lookup("limit")
+		fmt.Fprintf(w, "\t-l, --%s\t%s\n", f.Name, f.Usage)
+
+		fmt.Fprintf(w, "\nOutput Options:\n")
+		f = flag.Lookup("output")
+		fmt.Fprintf(w, "\t-o, --%s\t%s\n", f.Name, f.Usage)
+
+		fmt.Fprintf(w, "\nOptions for when output mode is display:\n")
+		f = flag.Lookup("width")
+		fmt.Fprintf(w, "\t-w, --%s\t%s\n", f.Name, f.Usage)
+		f = flag.Lookup("page")
+		fmt.Fprintf(w, "\t-p, --%s\t%s\n", f.Name, f.Usage)
+		f = flag.Lookup("no-ascii")
+		fmt.Fprintf(w, "\t-q, --%s\t%s\n", f.Name, f.Usage)
+		f = flag.Lookup("pretty")
+		fmt.Fprintf(w, "\t-y, --%s\t%s\n", f.Name, f.Usage)
+
+		fmt.Fprintf(w, "\nI/O Modes:\n")
+		fmt.Fprintf(w, "  * r, raw\tRaw bytes.\n")
+		fmt.Fprintf(w, "  * h, hex\tHex string.\n")
+		fmt.Fprintf(w, "  * s, base64\tBase64 string.\n")
+		fmt.Fprintf(w, "  * a, ascii\tAscii printable string with non-printables removed.\n")
+		fmt.Fprintf(w, "  * b, binary\tBinary string (ex: '0101').\n")
+		fmt.Fprintf(w, "  * d, display\tFormatted hex printout.\n")
+
+		fmt.Fprintf(w, "\nNote:\n")
+		fmt.Fprintf(w, "  * If no --file or --str set, will get input from stdin.\n")
+		fmt.Fprintf(w, "  * For any numeric args (ex: limit, offset, etc), values starting with:\n")
+		fmt.Fprintf(w, "      '0', '0x', '\\x', or, 'x' are parsed as hex instead of decimal.\n")
+		fmt.Fprintf(w, "    Same goes if value contains A-F or a-f.\n")
+
+		fmt.Fprintf(w, "\nExamples:\n\nTodo, some examples here. Include less -R and out to file via > \n")
+
+		// TODO: does ascii mode make sense? or turn it into a func?
+		// TODO: formatted vs raw binary in/out? or make func?
+		// TODO: unicode? utf8? ex: input raw output escaped unicode str?
+		// TODO: is the above unicode stuff a mode, or a function perhaps?
+		// TODO: what about raw binary vs formatted? display-hex vs display-bin?
+		// TODO: based on above decisions, finish help aside from examples.
+	}
+
+	flag.Parse()
+
+	// NOTE: defaults:
+	// output=display
+	// width=16
+	// page=0/none
+	// IF file/stdin, input=raw, if --str input=hex unless told otherwise by a nonempty mode
+
+	fmt.Printf("File is: %q, input: %q, mode: %q, out: %q\n", inFilename, inputStr, inMode, outMode)
+
+}
